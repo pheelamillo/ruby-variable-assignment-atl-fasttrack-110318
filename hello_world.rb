0732e9a4143cf18b6df19bda80c40820f7278f33
Pheelamillo Ghajiga <pheelamillo@gmail.com>
2019-02-19 23:36:20 +0000

    Automatically backed up by Learn

---
+++ hello_world.rb
@@ -0,0 +1,86 @@
+#puts "Welcome to fastrack"
+# puts "Welcome to fastrack"
+
+# def say_message(greeted)
+#   message = "Hello" + greeted.to_s + "great to meet you!"
+#   puts message 
+# end
+
+# 10.times do |step|
+#   say_message (step + )
+# end
+
+# def say_message(greeted)
+#   message = "Hello " + greeted.to_s + " great to meet you!"
+#   puts message 
+# end
+
+
+# x = 0
+# 10.times do |step|
+#   puts "We are on step " + step.to_s
+#   say_message ("brit")
+#   #only do this when step is 7
+#   if step = 7 
+#   puts "Hello we are on step"
+  
+# end
+# end
+
+# puts "x is " + x.to_s
+
+# if 6 == 6
+#   puts "Math is working "
+# end
+# if 6 == 7
+#   puts "call my bank"
+# end
+
+
+# write a program that asks input 
+# if the input matches your secret string, print a secret message 
+#   ortherwise print a normal message
+
+
+
+x = "secret\n"
+
+# puts "please enter you secret message:"
+# message = gets
+
+#   if message == x 
+#     puts "The Secret message is correct"
+#   elsif
+#   !(message ==x)
+#     puts "try again bruh!!"
+#   end 
+  
+  
+  # book1 = "Yam1"
+  # book2 = "Yam2"
+  # book3 = "Yam3"
+  
+  # nums = [1,2,3,4]
+  # nums[4] = 5
+  # books = [book1, book2, book3]
+  
+  # def print_names
+  # book1 = "Yam1"
+  # book2 = "Yam2"
+  # book3 = "Yam3"
+  # puts book1
+  # puts book2
+  # puts book3
+  # end
+  
+  # print_names
+  
+  def multiply_and_add_ten(first, second)
+    multiplied = first * second
+    result= multiplied + 10
+    result
+  end
+  answer = multiply_and_add_ten(8,15)
+  puts "The answer is " + answer.to_s
+  
+  
